3856841da8c5be95fe13a6f65498afd66b18b337
Efe Öge <efeoge@gmail.com>
2020-11-23 00:55:02 +0300

    Use turkish http proxies

--- mubier/utils.py
+++ mubier/utils.py
@@ -1,8 +1,25 @@
 import time
 import requests
+import random
 
 from bs4 import BeautifulSoup
 from datetime import datetime
+
+def get_proxy_address():
+    # https://www.proxynova.com/proxy-server-list/country-tr/
+    # https://spys.one/free-proxy-list/TR/
+    addresses = [
+        "88.255.60.220:8080",
+        "195.175.209.194:8080",
+        "95.0.66.71:1981",
+        "91.93.135.113:8080",
+        "176.236.99.204:8080",
+        "91.93.156.130:8080",
+        "176.235.182.79:8080",
+        "95.0.66.73:8080",
+        "80.253.247.42:3838"
+    ]
+    return random.choice(addresses)
 
 
 def get_today():
@@ -10,7 +27,16 @@ def get_today():
 
 
 def get_soup(url):
-    response = requests.get(url)
+    try:
+        proxy_address = get_proxy_address()
+        proxies = {
+            "http": proxy_address,
+            "https": proxy_address,
+        }
+        response = requests.get(url, proxies=proxies)
+    except requests.exceptions.ProxyError:
+        print(f"{url} - Proxy fail: {proxy_address}")
+        return get_soup(url)
     print(f"{url} - {response.status_code}")
     try:
         response.raise_for_status()
